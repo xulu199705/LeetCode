1e382bcbb723a13df0da3763dff6216750ce6b7d
DarkBlue <xulu199705@163.com>
2020-03-13 20:54:29 +0800

    updated 20200313

--- LeetCode_1370.py
+++ LeetCode_1370.py
@@ -2,5 +2,25 @@ from typing import List
 
 class Solution:
     def sortString(self, s: str) -> str:
+        ls = list(s)
+        ans = []
 
-        return ""
+        ls.sort()
+
+        while len(ls) > 0:
+            print(ls)
+            i, ans0 = 0, []
+            while i < len(ls):
+                it = ls[i]
+                print(it," ",i)
+                if ans0 == [] or ans0 != [] and ans0[len(ans0) - 1] != it:
+                    ans0.append(it)
+                    ls.remove(it)
+                else:
+                    i += 1
+            ans = ans + ans0
+
+            if ls == []: break
+            ls.reverse()
+
+        return "".join(ans)
